a73f09a2ff0eb7e4a8cafdb2897be5bc150bbdd4
Venus-God <148903558+Venus-God@users.noreply.github.com>
2024-09-02 20:21:55 +0200

    Add files via upload
    add models

---
+++ Models/Requests/Requests.cs
@@ -0,0 +1,7 @@
+﻿namespace TheTestApp.API.Models.Requests
+{
+    public class Requests
+    {
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TheTestApp.API.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+    }
+}
